7a35596cb1ac34544c941fbb1ebea6066d266fd5
grvrjt <gaurav1993rajput@gmail.com>
2022-12-18 12:56:06 +0530

    Feat:Order module added

--- e-commerce/backend/app.js
+++ e-commerce/backend/app.js
@@ -16,11 +16,15 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload())
 
-const product = require('./routes/productRoute')
-const user = require('./routes/userRoute')
-app.use('/api/v1', product);
-app.use('/api/v1', user);
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+// const payment = require("./routes/paymentRoute");
 
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+// app.use("/api/v1", payment);
 
 
 app.get('/api/v1', (req, res, next) => {
--- e-commerce/backend/controllers/userController.js
+++ e-commerce/backend/controllers/userController.js
@@ -8,11 +8,11 @@ const cloudinary = require('cloudinary')
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-  //   const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-  //     folder: "avatars",
-  //     width: 150,
-  //     crop: "scale",
-  //   });
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
 
   const { name, email, password } = req.body
   console.log(name, email, password)
